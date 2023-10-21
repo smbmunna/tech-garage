d461042885b5fc80c3b27310652b6f41592369ab
Mahmud Munna <smbmunna@gmail.com>
2023-10-21 19:13:24 +0600

    products page alignment fix

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -7,12 +7,12 @@ const Products = () => {
     //get the brand name
     const { brand } = useParams();
     if (products.length == 0) {
-        return <div className="h-screen"><h1 className="text-5xl font-bold text-center text-white mt-4">No Product Found!</h1></div>
+        return <div className="h-[50vh] flex items-center justify-center"><h1 className="text-5xl font-bold dark:text-black text-white mt-4">No Products Available!</h1></div>
     } else {
         return (
             <div className="max-w-6xl mx-auto">
                 <Slider />
-                <h1 className="text-3xl font-bold text-center text-white my-10">{brand} Products</h1>
+                <h1 className="text-3xl font-bold text-center dark:text-black text-white my-4">{brand} Products</h1>
                 {/* <div className="md:grid md:grid-cols-3 lg:grid-cols-3 justify-center gap-10 mx-auto"> */}
                 <div className="lg:grid lg:grid-cols-2 items-center gap-10 mx-auto w-4/5">
                     {
